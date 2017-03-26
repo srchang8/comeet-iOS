1e280039a9cd02f617d914fd342a53ffc1998bb9
Ricardo Contreras <ricardocontrerasrobles@gmail.com>
2017-03-26 10:10:23 -0700

    [BOOKAROOM-35] Add testLogin UI Test

--- comeetUITests/comeetUITests.swift
+++ comeetUITests/comeetUITests.swift
@@ -12,25 +12,50 @@ class comeetUITests: XCTestCase {
         
     override func setUp() {
         super.setUp()
-        
-        // Put setup code here. This method is called before the invocation of each test method in the class.
-        
-        // In UI tests it is usually best to stop immediately when a failure occurs.
         continueAfterFailure = false
-        // UI tests must launch the application that they test. Doing this in setup will make sure it happens for each test method.
         XCUIApplication().launch()
-
-        // In UI tests it’s important to set the initial state - such as interface orientation - required for your tests before they run. The setUp method is a good place to do this.
     }
     
     override func tearDown() {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
         super.tearDown()
     }
     
-    func testExample() {
-        // Use recording to get started writing UI tests.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    func testLogin() {
+
+        let app = XCUIApplication()
+        
+        if (app.buttons["Log out"].exists) {
+            app.buttons["Log out"].tap()
+        }
+        
+        app.buttons["Login"].tap()
+        
+        // Wait for ADAL SDK Web View to load
+        sleep(5)
+        
+        if (app.staticTexts["•••"].exists) {
+            app.staticTexts["•••"].tap()
+            app.staticTexts["Forget"].tap()
+        }
+        
+        app.buttons["Sign in"].tap()
+
+        let emailOrPhoneTextField = app.textFields["Email or phone"]
+        emailOrPhoneTextField.tap()
+        emailOrPhoneTextField.typeText("jablack@meetl.ink")
+        
+        let passwordSecureTextField = app.secureTextFields["Password"]
+        passwordSecureTextField.tap()
+        passwordSecureTextField.typeText("Comeet599")
+        
+        
+        app.children(matching: .window).element(boundBy: 0).children(matching: .other).element.tap()
+        app.buttons["Sign in"].tap()
+        
+        // Wait for dismiss and push animation
+        sleep(5)
+        
+        XCTAssert(app.navigationBars["Menu"].exists)
+
     }
-    
 }
