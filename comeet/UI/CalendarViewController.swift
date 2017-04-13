139efe53bf9ffd9d1e4c20066b2d99db13111084
StephenChang <srchang8@gmail.com>
2017-04-13 16:57:19 -0400

    [BOOKAROOM-42] - Added Pop up animation

--- comeet/UI/CalendarViewController.swift
+++ comeet/UI/CalendarViewController.swift
@@ -14,7 +14,7 @@ class CalendarViewController: BaseViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         self.view.backgroundColor = UIColor.black.withAlphaComponent(0.8)
-
+        self.showAnimate()
       
     }
 
@@ -23,10 +23,36 @@ class CalendarViewController: BaseViewController {
         // Dispose of any resources that can be recreated.
     }
     
-    //close calendar pop up
+    //close calendar pop up button
     @IBAction func done(_ sender: UIButton) {
-        self.view.removeFromSuperview()
+        //self.view.removeFromSuperview()
+        self.removeAnimate()
     }
     
 
+    //Animate pop up by sizing big then small
+    func showAnimate()
+    {
+        self.view.transform = CGAffineTransform(scaleX: 1.3, y: 1.3)
+        self.view.alpha = 0.0;
+        UIView.animate(withDuration: 0.25, animations: {
+            self.view.alpha = 1.0
+            self.view.transform = CGAffineTransform(scaleX: 1.0, y: 1.0)
+        });
+    }
+    
+    func removeAnimate()
+    {
+        UIView.animate(withDuration: 0.25, animations: {
+            self.view.transform = CGAffineTransform(scaleX: 1.3, y: 1.3)
+            self.view.alpha = 0.0;
+        }, completion:{(finished : Bool)  in
+            if (finished)
+            {
+                self.view.removeFromSuperview()
+            }
+        });
+    }
+
+    
 }
