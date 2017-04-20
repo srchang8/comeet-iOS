f7e3d4607213efa77248b15f876bb0050d00019b
srchang8 <srchang8@gmail.com>
2017-04-19 21:57:10 -0400

    [BOOKAROOM-41] My Agenda flow (#30)
    * [BOOKAROOM-41] - Added Pop up for date selection
    
    * [BOOKAROOM-42] - Added Pop up animation
    
    * [BOOKAROOM-42A] - Remove SSCalendar, Add WWCalendarTimeselector
    
    * [BOOKAROOM-42A] - Added Select date flow
    
    * [BOOKAROOM-41]  Added My Agenda flow

--- comeet/UI/MainMenu/MainMenuViewController.swift
+++ comeet/UI/MainMenu/MainMenuViewController.swift
@@ -16,13 +16,57 @@ class MainMenuViewController: BaseViewController, WWCalendarTimeSelectorProtocol
     
     var viewModel: MainMenuViewModel?
     
+    var agendaVC: MyAgendaViewController?
+    var roomListVC: RoomsListViewController?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        
+        //add swipe gesture recognizer to container view
+        let swipeGestureLeft = UISwipeGestureRecognizer(target: self, action: #selector(containerViewSwiped(gesture:)))
+        swipeGestureLeft.direction = UISwipeGestureRecognizerDirection.left
+        let swipeGestureRight = UISwipeGestureRecognizer(target: self, action: #selector(containerViewSwiped(gesture:)))
+        swipeGestureRight.direction = UISwipeGestureRecognizerDirection.right
+        containerView.addGestureRecognizer(swipeGestureLeft)
+        containerView.addGestureRecognizer(swipeGestureRight)
         setup()
     }
     
     override func viewWillAppear(_ animated: Bool) {
         self.navigationController?.isNavigationBarHidden = true
+    }
+    
+    func containerViewSwiped(gesture: UISwipeGestureRecognizer) {
+        switch gesture.direction {
+        case UISwipeGestureRecognizerDirection.left:
+            if agendaVC?.view.isHidden ?? true {
+                //animate fade in of agenda VC
+                agendaVC?.view.alpha = 0.0
+                agendaVC?.view.isHidden = false
+                UIView.animate(withDuration: 0.5, animations: {
+                    self.agendaVC?.view.alpha = 1.0
+                    self.roomListVC?.view.alpha = 0.0
+                }, completion: { (success) in
+                    self.roomListVC?.view.isHidden = true
+                })
+            }
+        case UISwipeGestureRecognizerDirection.right:
+            if roomListVC?.view.isHidden ?? true {
+                
+                //animate fade in of agenda VC
+                roomListVC?.view.alpha = 0.0
+                roomListVC?.view.isHidden = false
+                UIView.animate(withDuration: 0.5, animations: {
+                    self.roomListVC?.view.alpha = 1.0
+                    self.agendaVC?.view.alpha = 0.0
+                }, completion: { (success) in
+                    self.agendaVC?.view.isHidden = true
+                })
+            }
+        default:
+            break
+        }
     }
     
     override func viewWillDisappear(_ animated: Bool) {
@@ -67,6 +111,8 @@ class MainMenuViewController: BaseViewController, WWCalendarTimeSelectorProtocol
         calendarVC.optionCalendarBackgroundColorTodayHighlight = UIColor(colorLiteralRed: 129.0/255.0, green: 216.0/255.0, blue: 208.0/255.0, alpha: 1.0)
         calendarVC.optionSelectorPanelBackgroundColor = UIColor(colorLiteralRed: 129.0/255.0, green: 216.0/255.0, blue: 208.0/255.0, alpha: 1.0)
         calendarVC.optionButtonFontColorDone = UIColor(colorLiteralRed: 129.0/255.0, green: 216.0/255.0, blue: 208.0/255.0, alpha: 1.0)
+        calendarVC.optionCalendarBackgroundColorFutureDatesHighlight = UIColor(colorLiteralRed: 129.0/255.0, green: 216.0/255.0, blue: 208.0/255.0, alpha: 1.0)
+        calendarVC.optionCalendarBackgroundColorPastDatesHighlight = UIColor(colorLiteralRed: 129.0/255.0, green: 216.0/255.0, blue: 208.0/255.0, alpha: 1.0)
         calendarVC.delegate = self
         calendarVC.modalPresentationStyle = .popover
         calendarVC.popoverPresentationController?.sourceView = sender as? UIButton
@@ -99,10 +145,23 @@ private extension MainMenuViewController {
         
         dateButton.setTitle(viewModel?.selectedDate.displayStringDate(), for: .normal)
         addRoomsList()
+        addAgendaView()
+        
+    }
+    
+    func addAgendaView() {
+        if let agendaVC = getAgendaVC() {
+            addChild(viewController: agendaVC, inView: containerView)
+            self.agendaVC = agendaVC
+            //hide agenda view initially
+            agendaVC.view.isHidden = true
+        }
     }
     
     func addRoomsList() {
         if let roomsListVC = getRoomsListVC() {
+            // have a reference to the rooms list view controller
+            self.roomListVC = roomsListVC
             addChild(viewController: roomsListVC, inView: containerView)
         }
     }
@@ -132,6 +191,17 @@ private extension MainMenuViewController {
         }
         
         return roomsListVC
+    }
+    
+    //create a reference of Agenda VC
+    func getAgendaVC() -> MyAgendaViewController? {
+        let storyboard = UIStoryboard(name: "Main", bundle: nil)
+        let viewController = storyboard.instantiateViewController(withIdentifier: "MyAgendaViewController")
+        guard let meetingsVC = viewController as? MyAgendaViewController else {
+            return nil
+        }
+        
+        return meetingsVC
     }
     
     func getRoomsListViewModel() -> RoomsListViewModel? {
